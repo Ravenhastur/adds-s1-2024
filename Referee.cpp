29c0f1a0d7134048777fbdd2d05ce9f847ad5394
Ravenhastur <112268698+Ravenhastur@users.noreply.github.com>
2024-03-04 13:58:03 +1030

    abc

--- Referee.cpp
+++ Referee.cpp
@@ -1,5 +1,5 @@
+// Referee.cpp
 #include "Referee.h"
-#include <iostream>
 
 Referee::Referee() {}
 
@@ -7,16 +7,11 @@ Player* Referee::refGame(Player* player1, Player* player2) {
     char move1 = player1->makeMove();
     char move2 = player2->makeMove();
 
-    if (move1 == move2) {
-        std::cout << "It's a Tie." << std::endl;
-        return nullptr;
-    } else if ((move1 == 'R' && move2 == 'S') ||
-               (move1 == 'S' && move2 == 'P') ||
-               (move1 == 'P' && move2 == 'R')) {
-        std::cout << player1->getName() << " Wins." << std::endl;
-        return player1;
-    } else {
-        std::cout << player2->getName() << " Wins." << std::endl;
-        return player2;
-    }
+    if (move1 == move2)
+        return nullptr; // Tie
+
+    if ((move1 == 'R' && move2 == 'S') || (move1 == 'S' && move2 == 'P') || (move1 == 'P' && move2 == 'R'))
+        return player1; // Player 1 wins
+
+    return player2; // Player 2 wins
 }
