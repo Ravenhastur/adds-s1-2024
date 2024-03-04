9e6b98de537904075c558e5f84315d8601df050f
Ravenhastur <112268698+Ravenhastur@users.noreply.github.com>
2024-03-04 13:50:09 +1030

    abc

--- main.cpp
+++ main.cpp
@@ -1,19 +1,23 @@
+#include <iostream>
+#include <vector>
 #include "Human.h"
 #include "Computer.h"
 #include "Referee.h"
 
+using namespace std;
+
 int main() {
-    Player* human = new Human("Mei");
-    Player* computer = new Computer();
+    Human me("Mei");
+    Computer computer;
     Referee referee;
 
-    Player* winner = referee.refGame(human, computer);
-    if (winner != nullptr) {
-        std::cout << "The winner is: " << winner->getName() << std::endl;
-    }
+    Player* winner = referee.refGame(&me, &computer);
 
-    delete human;
-    delete computer;
+    if (winner == nullptr) {
+        cout << "It's a Tie" << endl;
+    } else {
+        cout << winner->getName() << " Wins" << endl;
+    }
 
     return 0;
 }
