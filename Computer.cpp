0a16cab9c2753717da5c6e8ca73867ac49a5097c
Ravenhastur <112268698+Ravenhastur@users.noreply.github.com>
2024-03-11 04:05:30 +1030

    s

--- Computer.cpp
+++ Computer.cpp
@@ -1,10 +1,39 @@
 #include "Computer.h"
-#include "Rock.h" // Include move headers here
+#include <cstdlib> // For rand() and srand()
+#include <ctime>   // For time()
 
+using namespace std;
+
+Computer::Computer() {
+    // Seed the random number generator with current time
+    srand(time(nullptr));
+}
 
 Move* Computer::makeMove() {
-    // Computer always plays Rock for simplicity
-    return new Rock();
+    // Generate a random number between 0 and 7 (inclusive)
+    int randomMove = rand() % 8;
+
+    // Return corresponding move object based on random number
+    switch (randomMove) {
+        case 0:
+            return new Rock();
+        case 1:
+            return new Paper();
+        case 2:
+            return new Scissors();
+        case 3:
+            return new Monkey();
+        case 4:
+            return new Robot();
+        case 5:
+            return new Pirate();
+        case 6:
+            return new Ninja();
+        case 7:
+            return new Zombie();
+        default:
+            return nullptr; // This should never happen
+    }
 }
 
 string Computer::getName() {
--- Human.cpp
+++ Human.cpp
@@ -1,7 +1,5 @@
 #include "Human.h"
 #include <iostream>
-#include "Rock.h" // Include move headers here
-// Include other move headers as well
 
 using namespace std;
 
@@ -9,17 +7,32 @@ Human::Human(string name) : name(name) {}
 
 Move* Human::makeMove() {
     string moveName;
-    cout << "Enter Move: ";
+    cout << "Enter Move for " << name << ": ";
     cin >> moveName;
 
     // Return corresponding move object based on input
     if (moveName == "Rock")
         return new Rock();
-    // Handle other moves similarly
+    else if (moveName == "Paper")
+        return new Paper();
+    else if (moveName == "Scissors")
+        return new Scissors();
+    else if (moveName == "Monkey")
+        return new Monkey();
+    else if (moveName == "Robot")
+        return new Robot();
+    else if (moveName == "Pirate")
+        return new Pirate();
+    else if (moveName == "Ninja")
+        return new Ninja();
+    else if (moveName == "Zombie")
+        return new Zombie();
 
     return nullptr; // Return nullptr for invalid move
 }
 
 string Human::getName() {
     return name;
+}
+
 }
