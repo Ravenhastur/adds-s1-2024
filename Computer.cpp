f0e8f20db8353242891b876a5e3ed10eef698bff
Ravenhastur <112268698+Ravenhastur@users.noreply.github.com>
2024-03-11 04:48:16 +1030

    s

--- Computer.cpp
+++ Computer.cpp
@@ -2,18 +2,41 @@
 #include <cstdlib> // For rand() and srand()
 #include <ctime>   // For time()
 
-Computer::Computer() {
-    // Seed the random number generator with current time
-    srand(time(nullptr));
-}
+using namespace std;
+
+// Constructor definition
+Computer::Computer() {}
 
 Move* Computer::makeMove() {
+    // Seed the random number generator with current time
+    srand(time(nullptr));
+
     // Generate a random number between 0 and 7 (inclusive)
     int randomMove = rand() % 8;
-    // Implement logic to return corresponding move object based on random number
-    return nullptr; // Placeholder
+
+    // Return corresponding move object based on random number
+    switch (randomMove) {
+        case 0:
+            return new Rock();
+        case 1:
+            return new Paper();
+        case 2:
+            return new Scissors();
+        case 3:
+            return new Monkey();
+        case 4:
+            return new Robot();
+        case 5:
+            return new Pirate();
+        case 6:
+            return new Ninja();
+        case 7:
+            return new Zombie();
+        default:
+            return nullptr; // This should never happen
+    }
 }
 
-std::string Computer::getName() {
+string Computer::getName() {
     return "Computer";
 }
--- Referee.cpp
+++ Referee.cpp
@@ -5,9 +5,21 @@ Referee::Referee() {}
 Player* Referee::refGame(Player* player1, Player* player2) {
     Move* move1 = player1->makeMove();
     Move* move2 = player2->makeMove();
-    // Implement logic to determine the winner based on moves
-    // Return the winning player or nullptr for a tie
-    return nullptr; // Placeholder
-}
 
+    // Determine the names of the moves made by players
+    string moveName1 = move1->getName();
+    string moveName2 = move2->getName();
+
+    // Determine the winner based on the moves
+    // Add your logic here to determine the winner
+    // According to the rules of the Moropinzee game
+
+    // Temporary logic: Assume it's a tie for now
+    Player* winner = nullptr;
+
+    // Free memory
+    delete move1;
+    delete move2;
+
+    return winner;
 }
