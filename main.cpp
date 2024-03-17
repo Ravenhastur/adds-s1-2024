3d1690f6388a8237322cd440f3b1c2264253c749
Ravenhastur <112268698+Ravenhastur@users.noreply.github.com>
2024-03-18 00:25:37 +1030

    d

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Truckloads.h"
+
+using namespace std;
+
+int main() {
+    Truckloads truckloads;
+    cout << truckloads.numTrucks(14, 3) << endl; 
+    return 0;
+}
