b9f167ef1861b98a25501230611cfdd2a948f06f
Ravenhastur <112268698+Ravenhastur@users.noreply.github.com>
2024-03-28 18:08:27 +1030

    s

--- Finder.cpp
+++ Finder.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 vector<int> Finder::findSubstrings(string s1, string s2) {
 vector<int> result;
-int found = s1.find(s2);
 for(size_t i = 1; i <= s2.size(); i++) {
 size_t found = s1.find(s2.substr(0, i));
 if (found != string::npos) {
-result.push_back(found);
 i = found;
+result.push_back(found);
+
 } else {
 result.push_back(-1);
 }
