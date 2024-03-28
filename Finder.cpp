013467cd98a7821eaa50b5112aa6d76d5e7b4f52
Ravenhastur <112268698+Ravenhastur@users.noreply.github.com>
2024-03-28 17:42:39 +1030

    s

--- Finder.cpp
+++ Finder.cpp
@@ -1,25 +1,27 @@
-vector<int> Finder::findSubstrings(string s1, string s2)
+#include "Finder.h"
+
+std::vector<int> Finder::findSubstrings(std::string s1, std::string s2)
 {
-    vector<int> result;
-    string prevSubstring;  // Store the previous substring
-    size_t prevFound = string::npos;  // Result of the previous search
-    size_t currFound = string::npos;  // Result of the current search
+    std::vector<int> result;
+    std::string prevSubstring;
+    size_t prevFound = std::string::npos;
+    size_t currFound = std::string::npos;
 
     for (size_t i = 1; i <= s2.size(); i++) {
-        string currSubstring = s2.substr(0, i);  // Store the current substring
+        std::string currSubstring = s2.substr(0, i);
 
         if (currSubstring != prevSubstring) {
-            currFound = s1.find(currSubstring);  // Search only if the substring changes
+            currFound = s1.find(currSubstring);
         }
 
-        if (currFound != string::npos) {
+        if (currFound != std::string::npos) {
             result.push_back(currFound);
         } else {
             result.push_back(-1);
         }
 
-        prevSubstring = currSubstring;  // Update the previous substring
-        prevFound = currFound;  // Update the previous search result
+        prevSubstring = currSubstring;
+        prevFound = currFound;
     }
 
     return result;
