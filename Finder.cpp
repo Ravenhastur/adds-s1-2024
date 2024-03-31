b1927b93e466d53cde45b1a5640aa5c8ea52ad79
Ravenhastur <112268698+Ravenhastur@users.noreply.github.com>
2024-04-01 04:48:46 +1030

    d

--- Finder.cpp
+++ Finder.cpp
@@ -1,21 +1,3 @@
-// #include "Finder.h"
-
-// using namespace std;
-
-// vector<int> Finder::findSubstrings(string s1, string s2) {
-//     vector<int> result;
-//     for(uint i = 1; i <= s2.size(); i++){
-//         int found = s1.find(s2.substr(0, i));
-
-//         if (found != string::npos) {
-//             result.push_back(found);
-//         } else {
-//             result.push_back(-1);
-//         }
-//     }
-//     return result;
-// }
-
 #include "Finder.h"
 #include <vector>
 
@@ -41,3 +23,29 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
 
     return result;
 }
+
+// #include "Finder.h"
+// #include <vector>
+
+// using namespace std;
+
+// vector<int> Finder::findSubstrings(string s1, string s2) {
+//     vector<int> result;
+//     int n = s2.size();
+//     int pos = 0;
+
+//     for (int i = 0; i < n; i++) {
+//         string prefix = s2.substr(0, i + 1);
+//         int found = s1.find(prefix, pos);
+
+//         if (found == string::npos) {
+//             result.push_back(-1);
+//             break; // No need to search for longer prefixes
+//         } else {
+//             result.push_back(found);
+//             pos = found + 1; // Start searching from the next position
+//         }
+//     }
+
+//     return result;
+// }
