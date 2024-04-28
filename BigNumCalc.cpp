132fc92cce4e41ee299d81ca8c5b4654a557e49f
Ravenhastur <112268698+Ravenhastur@users.noreply.github.com>
2024-04-29 06:41:40 +0930

    s

--- BigNumCalc.cpp
+++ BigNumCalc.cpp
@@ -1,112 +1,83 @@
+
 #include "BigNumCalc.h"
 
-std::list<int> BigNumCalc::buildBigNum(const std::string& numString) {
-    std::list<int> result;
-    for (char digit : numString) {
-        result.push_back(digit - '0');  // Convert char to int and add to the list
+std::list<int> BigNumCalc::buildBigNum(std::string numString) {
+    std::list<int> bigNum;
+    for (char c : numString) {
+        bigNum.push_front(c - '0');
     }
+    while (!bigNum.empty() && bigNum.front() == 0) {
+        bigNum.pop_front();
+    }
+    return bigNum;
+}
+
+std::list<int> BigNumCalc::add(std::list<int> num1, std::list<int> num2) {
+    std::list<int> result;
+    int carry = 0;
+    auto it1 = num1.rbegin(), it2 = num2.rbegin();
+
+    while (it1 != num1.rend() || it2 != num2.rend()) {
+        int sum = carry;
+        if (it1 != num1.rend()) {
+            sum += *it1++;
+        }
+        if (it2 != num2.rend()) {
+            sum += *it2++;
+        }
+        result.push_front(sum % 10);
+        carry = sum / 10;
+    }
+
+    if (carry) {
+        result.push_front(carry);
+    }
+
     return result;
 }
 
-std::list<int> BigNumCalc::add(const std::list<int>& num1, const std::list<int>& num2) {
-    std::list<int> sum;
-    int carry = 0;
-
-    std::list<int>::const_reverse_iterator it1 = num1.rbegin();
-    std::list<int>::const_reverse_iterator it2 = num2.rbegin();
-
-    while (it1 != num1.rend() || it2 != num2.rend() || carry != 0) {
-        int digitSum = carry;
-
-        if (it1 != num1.rend()) {
-            digitSum += *it1;
-            ++it1;
-        }
-
-        if (it2 != num2.rend()) {
-            digitSum += *it2;
-            ++it2;
-        }
-
-        carry = digitSum / 10;
-        digitSum %= 10;
-        sum.push_front(digitSum);
-    }
-
-    return sum;
-}
-
-std::list<int> BigNumCalc::sub(const std::list<int>& num1, const std::list<int>& num2) {
-    std::list<int> difference;
+std::list<int> BigNumCalc::sub(std::list<int> num1, std::list<int> num2) {
+    std::list<int> result;
     int borrow = 0;
+    auto it1 = num1.rbegin(), it2 = num2.rbegin();
 
-    std::list<int>::const_reverse_iterator it1 = num1.rbegin();
-    std::list<int>::const_reverse_iterator it2 = num2.rbegin();
-
-    while (it1 != num1.rend() || it2 != num2.rend()) {
-        int digitDiff = borrow;
-
-        if (it1 != num1.rend()) {
-            digitDiff += *it1;
-            ++it1;
-        }
-
+    while (it1 != num1.rend()) {
+        int diff = *it1 - borrow;
         if (it2 != num2.rend()) {
-            digitDiff -= *it2;
-            ++it2;
+            diff -= *it2++;
         }
-
-        if (digitDiff < 0) {
-            digitDiff += 10;
-            borrow = -1;
+        if (diff < 0) {
+            diff += 10;
+            borrow = 1;
         } else {
             borrow = 0;
         }
-
-        difference.push_front(digitDiff);
+        result.push_front(diff);
+        it1++;
     }
 
-    // Remove leading zeroes
-    while (!difference.empty() && difference.front() == 0) {
-        difference.pop_front();
+    while (!result.empty() && result.front() == 0) {
+        result.pop_front();
     }
 
-    return difference;
+    return result;
 }
 
-std::list<int> BigNumCalc::mul(const std::list<int>& num1, const std::list<int>& num2) {
-    std::list<int> product;
+std::list<int> BigNumCalc::mul(std::list<int> num1, std::list<int> num2) {
+    std::list<int> result;
+    int carry = 0;
+    int multiplier = num2.front();
+    num2.pop_front();
 
-    for (int i = 0; i < num2.size(); ++i) {
-        int singleDigit = *(num2.rbegin());
-        std::list<int> tempProduct(i, 0);
-
-        int carry = 0;
-
-        std::list<int>::const_reverse_iterator it = num1.rbegin();
-
-        while (it != num1.rend() || carry != 0) {
-            int digitProduct = carry;
-
-            if (it != num1.rend()) {
-                digitProduct += *it * singleDigit;
-                ++it;
-            }
-
-            carry = digitProduct / 10;
-            digitProduct %= 10;
-            tempProduct.push_front(digitProduct);
-        }
-
-        product = add(product, tempProduct);
-
-        num2.pop_back();
+    for (int digit : num1) {
+        int product = digit * multiplier + carry;
+        result.push_back(product % 10);
+        carry = product / 10;
     }
 
-    // Remove leading zeroes
-    while (!product.empty() && product.front() == 0) {
-        product.pop_front();
+    if (carry) {
+        result.push_back(carry);
     }
 
-    return product;
+    return result;
 }
