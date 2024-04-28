73196dc3f1ef51a1a7e1759ac962cb177ec7f967
Ravenhastur <112268698+Ravenhastur@users.noreply.github.com>
2024-04-29 06:46:13 +0930

    s

--- BigNumCalc.cpp
+++ BigNumCalc.cpp
@@ -1,18 +1,43 @@
-
 #include "BigNumCalc.h"
 
 std::list<int> BigNumCalc::buildBigNum(std::string numString) {
     std::list<int> bigNum;
     for (char c : numString) {
-        bigNum.push_front(c - '0');
-    }
-    while (!bigNum.empty() && bigNum.front() == 0) {
-        bigNum.pop_front();
+        if (isdigit(c)) {
+            bigNum.push_front(c - '0');
+        }
     }
     return bigNum;
 }
 
 std::list<int> BigNumCalc::add(std::list<int> num1, std::list<int> num2) {
+    return addLargeDigits(num1, num2);
+}
+
+std::list<int> BigNumCalc::sub(std::list<int> num1, std::list<int> num2) {
+    return subLargeDigits(num1, num2);
+}
+
+std::list<int> BigNumCalc::mul(std::list<int> num1, std::list<int> num2) {
+    std::list<int> result;
+    int carry = 0;
+    int multiplier = num2.front();
+    num2.pop_front();
+
+    for (int digit : num1) {
+        int product = digit * multiplier + carry;
+        result.push_back(product % 10);
+        carry = product / 10;
+    }
+
+    if (carry) {
+        result.push_back(carry);
+    }
+
+    return result;
+}
+
+std::list<int> BigNumCalc::addLargeDigits(std::list<int> num1, std::list<int> num2) {
     std::list<int> result;
     int carry = 0;
     auto it1 = num1.rbegin(), it2 = num2.rbegin();
@@ -36,7 +61,7 @@ std::list<int> BigNumCalc::add(std::list<int> num1, std::list<int> num2) {
     return result;
 }
 
-std::list<int> BigNumCalc::sub(std::list<int> num1, std::list<int> num2) {
+std::list<int> BigNumCalc::subLargeDigits(std::list<int> num1, std::list<int> num2) {
     std::list<int> result;
     int borrow = 0;
     auto it1 = num1.rbegin(), it2 = num2.rbegin();
@@ -58,25 +83,6 @@ std::list<int> BigNumCalc::sub(std::list<int> num1, std::list<int> num2) {
 
     while (!result.empty() && result.front() == 0) {
         result.pop_front();
-    }
-
-    return result;
-}
-
-std::list<int> BigNumCalc::mul(std::list<int> num1, std::list<int> num2) {
-    std::list<int> result;
-    int carry = 0;
-    int multiplier = num2.front();
-    num2.pop_front();
-
-    for (int digit : num1) {
-        int product = digit * multiplier + carry;
-        result.push_back(product % 10);
-        carry = product / 10;
-    }
-
-    if (carry) {
-        result.push_back(carry);
     }
 
     return result;
