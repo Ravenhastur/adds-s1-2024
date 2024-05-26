2f76caac9d28de9084578480d113b064a5cd7c01
Ravenhastur <112268698+Ravenhastur@users.noreply.github.com>
2024-05-26 18:50:27 +0930

    s

--- DocumentManager.cpp
+++ DocumentManager.cpp
@@ -1,42 +1,35 @@
 #include "DocumentManager.h"
 
-void DocumentManager::addDocument(std::string name, int id, int license_limit) {
-    Document document;
-    document.id = id;
-    document.license_limit = license_limit;
-    document.current_borrowers = 0;
-    documents[name] = document;
+Document::Document(const std::string& name, int id, int license_limit)
+    : name(name), id(id), license_limit(license_limit) {}
+
+void DocumentManager::addDocument(const std::string& name, int id, int license_limit) {
+    nameToDocID[name] = id;
+    docIDToDocument[id] = Document(name, id, license_limit);
 }
 
 void DocumentManager::addPatron(int patronID) {
     patrons.insert(patronID);
 }
 
-int DocumentManager::search(std::string name) {
-    if (documents.count(name) > 0) {
-        return documents[name].id;
-    }
-    return 0;
+int DocumentManager::search(const std::string& name) {
+    auto it = nameToDocID.find(name);
+    return (it != nameToDocID.end()) ? it->second : 0;
 }
 
 bool DocumentManager::borrowDocument(int docid, int patronID) {
-    if (documents.count(docid) > 0 && patrons.count(patronID) > 0) {
-        Document& document = documents[docid];
-        if (document.current_borrowers < document.license_limit) {
-            borrowed_documents[patronID].insert(docid);
-            document.current_borrowers++;
-            return true;
-        }
-    }
-    return false;
+    if (patrons.count(patronID) == 0)
+        return false; // Invalid patron ID
+
+    auto& doc = docIDToDocument[docid];
+    if (doc.borrowedBy.size() >= doc.license_limit)
+        return false; // Reached license limit
+
+    doc.borrowedBy.insert(patronID);
+    return true;
 }
 
 void DocumentManager::returnDocument(int docid, int patronID) {
-    if (documents.count(docid) > 0 && patrons.count(patronID) > 0) {
-        Document& document = documents[docid];
-        if (borrowed_documents[patronID].count(docid) > 0) {
-            borrowed_documents[patronID].erase(docid);
-            document.current_borrowers--;
-        }
-    }
+    auto& doc = docIDToDocument[docid];
+    doc.borrowedBy.erase(patronID);
 }
--- DocumentManager.h
+++ DocumentManager.h
@@ -1,28 +1,33 @@
-#ifndef DOCUMENTMANAGER_H
-#define DOCUMENTMANAGER_H
+#ifndef DOCUMENT_MANAGER_H
+#define DOCUMENT_MANAGER_H
 
-#include <string>
 #include <unordered_map>
 #include <unordered_set>
+#include <string>
+#include <vector>
 
-class DocumentManager {
+class Document {
 public:
-    void addDocument(std::string name, int id, int license_limit);
-    void addPatron(int patronID);
-    int search(std::string name);
-    bool borrowDocument(int docid, int patronID);
-    void returnDocument(int docid, int patronID);
+    std::string name;
+    int id;
+    int license_limit;
+    std::unordered_set<int> borrowedBy; // Set of patron IDs who have borrowed this document
 
-private:
-    struct Document {
-        std::string name;
-        int id;
-        int license_limit;
-        int borrowed_count;
-    };
-
-    std::unordered_map<int, Document> documents;
-    std::unordered_set<int> patrons;
+    Document(const std::string& name, int id, int license_limit);
 };
 
-#endif // DOCUMENTMANAGER_H
+class DocumentManager {
+private:
+    std::unordered_map<std::string, int> nameToDocID; // Map document name to document ID
+    std::unordered_map<int, Document> docIDToDocument; // Map document ID to Document object
+    std::unordered_set<int> patrons; // Set of registered patrons
+
+public:
+    void addDocument(const std::string& name, int id, int license_limit);
+    void addPatron(int patronID);
+    int search(const std::string& name);
+    bool borrowDocument(int docid, int patronID);
+    void returnDocument(int docid, int patronID);
+};
+
+#endif // DOCUMENT_MANAGER_H
